9a95d6a681ee37a0bdbe52d894fb32da80e811c9
minarai847 <72494683+minarai847@users.noreply.github.com>
2020-10-26 14:57:10 +0900

    ミスを発見した為、再提出

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show] 
+  before_action :move_to_index, only: [:edit]
+  
   def index
     @prototypes=Prototype.all
   end
@@ -28,9 +30,6 @@ class PrototypesController < ApplicationController
 
     def edit
       @prototype = Prototype.find(params[:id])
-      unless @prototype.user_id == current_user.id
-        redirect_to action: :index
-      end
     end
 
     def update
@@ -55,5 +54,11 @@ class PrototypesController < ApplicationController
 
    def prototype_params
     params.require(:prototype).permit(:title, :catch_copy,:concept,:image).merge(user_id: current_user.id)
+   end
+  def move_to_index
+    @prototype = Prototype.find(params[:id])
+    unless @prototype.user_id == current_user.id
+      redirect_to action: :index
+    end
   end
 end
